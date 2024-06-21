e2638a27d836602661989882e58a7afbd69afb74
Andrés Torres <camitorres1404@gmail.com>
2024-06-21 16:55:08 -0500

    feat: update footer styles and create landing layout

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,159 +1,138 @@
 <template>
-  <div style="background: #6855f9; margin-top: -9rem">
-    <div class="footer">
-      <div class="footer__information-container">
-        <p class="footer__title">
-          EXPERTO EN TRANSFORMACIÓN DIGITAL PARA INDUSTRIAS
-        </p>
-        <div class="footer__contacto">
-          <div class="d-flex my-5">
-            <div class="footer__img-container"></div>
-            <div class="footer__text-container">
-              <p class="footer__text">Contáctame</p>
-            </div>
-          </div>
-          <div class="d-flex">
-            <div class="footer__img-container">
-              <img src="@/assets/img/img12.png" />
-            </div>
-            <div class="footer__text-container">
-              <div class="footer__row-container">
-                <div>
-                  <img src="@/assets/img/icon/icon01.png" />
-                </div>
-                <div class="mx-3">
-                  <p class="footer__description">{{ email }}</p>
-                </div>
-              </div>
-              <div class="footer__row-container">
-                <div>
-                  <img src="@/assets/img/icon/icon02.png" />
-                </div>
-                <div class="mx-3">
-                  <p class="footer__description">Dirección: {{ address }}</p>
-                </div>
-              </div>
-              <div class="footer__row-container">
-                <div>
-                  <img src="@/assets/img/icon/icon03.png" />
-                </div>
-                <div class="mx-3">
-                  <p class="footer__description">@jairmanrico</p>
-                </div>
-              </div>
-              <div class="d-flex" style="align-items: center">
-                <div class="ma-0 pa-0" :cols="12">
-                  <v-btn
-                    rounded="xl"
-                    variant="text"
-                    target="_blank"
-                    :href="linkedin"
-                  >
-                    <img src="@/assets/img/icon/socialIcons01.png" />
-                  </v-btn>
-                  <v-btn
-                    rounded="xl"
-                    variant="text"
-                    target="_blank"
-                    :href="youtube"
-                  >
-                    <img src="@/assets/img/icon/socialIcons02.png" />
-                  </v-btn>
-                  <v-btn
-                    rounded="xl"
-                    variant="text"
-                    target="_blank"
-                    :href="github"
-                  >
-                    <img src="@/assets/img/icon/socialIcons03.png" />
-                  </v-btn>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="d-flex">
-            <div class="footer__img-container"></div>
+  <div class="bg-primary">
+    <div class="py-10 lg:mx-[6rem] mx-[3rem] flex flex-col">
+      <div class="flex flex-col md:flex-row justify-between">
+        <div class="mb-10 lg:mb-0">
+          <p class="text-2xl font-bold text-white mb-5">
+            EXPERTO EN TRANSFORMACIÓN DIGITAL PARA INDUSTRIAS
+          </p>
+          <div class="flex items-center mb-5">
             <div>
-              <p class="footer__description my-10">
-                © {{ new Date().getFullYear() }} Divelia SAC.
-              </p>
+              <p class="text-white font-semibold">Contáctame</p>
             </div>
+          </div>
+          <div class="mb-5">
+            <div class="flex items-center mb-3">
+              <img
+                src="@/assets/img/icon/icon01.png"
+                alt="Email Icon"
+                class="mr-3"
+              />
+              <p class="text-white">{{ email }}</p>
+            </div>
+            <div class="flex items-center mb-3">
+              <img
+                src="@/assets/img/icon/icon02.png"
+                alt="Address Icon"
+                class="mr-3"
+              />
+              <p class="text-white">Dirección: {{ address }}</p>
+            </div>
+            <div class="flex items-center mb-5">
+              <img
+                src="@/assets/img/icon/icon03.png"
+                alt="Social Icon"
+                class="mr-3"
+              />
+              <p class="text-white">@jairmanrico</p>
+            </div>
+            <div class="flex space-x-4 mt-4 items-end">
+              <a :href="linkedin" target="_blank">
+                <img
+                  src="@/assets/img/icon/socialIcons01.png"
+                  alt="LinkedIn"
+                  class="w-8"
+                />
+              </a>
+              <a :href="youtube" target="_blank">
+                <img
+                  src="@/assets/img/icon/socialIcons02.png"
+                  alt="YouTube"
+                  class="w-8"
+                />
+              </a>
+              <a :href="github" target="_blank">
+                <img
+                  src="@/assets/img/icon/socialIcons03.png"
+                  alt="GitHub"
+                  class="w-8"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p class="text-2xl font-bold text-white mb-5">Blogs</p>
+          <div class="space-y-4">
+            <div
+              v-for="(blog, bg) in blogs"
+              :key="bg"
+              class="bg-[#6855F9] p-4 rounded-lg text-white flex space-x-4"
+            >
+              <div class="flex-shrink-0 self-center">
+                <img
+                  :src="blog.image"
+                  class="w-24 h-24 object-cover rounded-lg"
+                />
+              </div>
+              <div>
+                <p class="text-lg font-semibold" v-html="blog.title"></p>
+                <p class="text-sm mt-2" v-html="blog.short_description"></p>
+                <p class="text-xs mt-2" v-html="blog.date"></p>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center mt-6">
+            <nuxt-link
+              class="text-primary bg-white py-2 px-6 rounded-full text-lg font-semibold"
+              to="/blogs"
+            >
+            Ver más
+            </nuxt-link>
           </div>
         </div>
       </div>
-      <div class="footer__information-container">
-        <p class="footer__text">Blogs</p>
-        <div class="footer__blog">
-          <v-card
-            color="#6855F9"
-            theme="dark"
-            class="d-flex blog"
-            v-for="(blog, bg) in blogs"
-            :key="bg"
-          >
-            <div class="blog__img">
-              <img :src="blog.image" style="width: 90%" />
-            </div>
-            <div class="blog__content">
-              <p class="blog__title" v-html="blog.title" />
-              <p class="blog__description my-4" v-html="blog.short_description" />
-              <p class="blog__fecha" v-html="blog.date" />
-            </div>
-          </v-card>
-        </div>
-        <div class="d-flex" style="justify-content: center">
-          <v-btn
-            class="text-indigo-accent-4 text-subtitle-1"
-            height="35px"
-            rounded="xl"
-            color="#FFF"
-            style="padding: 0 3rem"
-          >
-            Ver más
-          </v-btn>
-        </div>
+      <div>
+        <p class="text-white text-center mt-5">© {{ new Date().getFullYear() }} Divelia SAC.</p>
       </div>
     </div>
   </div>
 </template>
-<script>
-import axios from "axios";
-export default {
-    name: 'TheFooter',
-  data: () => ({
-    perfil: null,
-    address: null,
-    email: null,
-    phone: null,
-    linkedin: null,
-    youtube: null,
-    github: null,
-    blogs: null,
-  }),
-  methods: {
-    async getPerfil() {
-      let response = await axios.get(
-        "https://admin.jairmanrique.com/api/v1/portafolio/perfil/"
-      );
-      this.perfil = response.data;
-      this.email = this.perfil.email;
-      this.phone = this.perfil.phone;
-      this.address = this.perfil.address;
-      this.youtube = this.perfil.youtube;
-      this.github = this.perfil.github;
-      this.linkedin = this.perfil.linkedin;
-    },
 
-    async getBlogs() {
-      let response = await axios.get(
-        "https://admin.jairmanrique.com/api/v1/portafolio/blogs/"
-      );
-      this.blogs = response.data;
+<script>
+  import axios from "axios";
+  export default {
+    name: "TheFooter",
+    data: () => ({
+      perfil: null,
+      address: null,
+      email: null,
+      phone: null,
+      linkedin: null,
+      youtube: null,
+      github: null,
+      blogs: null,
+    }),
+    methods: {
+      async getPerfil() {
+        let response = await this.$api.get("/portafolio/perfil/");
+        this.perfil = response.data;
+        this.email = this.perfil.email;
+        this.phone = this.perfil.phone;
+        this.address = this.perfil.address;
+        this.youtube = this.perfil.youtube;
+        this.github = this.perfil.github;
+        this.linkedin = this.perfil.linkedin;
+      },
+
+      async getBlogs() {
+        let response = await this.$api.get("/portafolio/blogs/");
+        this.blogs = response.data;
+      },
     },
-  },
-  mounted() {
-    this.getPerfil();
-    this.getBlogs();
-  },
-};
+    mounted() {
+      this.getPerfil();
+      this.getBlogs();
+    },
+  };
 </script>
---
+++ layouts/Landing.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col gap-5 h-[100%] min-h-screen">
+    <the-header />
+    <div class="flex-grow flex items-center">
+      <slot />
+    </div>
+    <the-footer />
+  </div>
+</template>
+
+<script>
+  import TheHeader from "@/components/TheHeader.vue";
+  import TheFooter from "@/components/TheFooter.vue";
+  export default {
+    components: {
+      TheHeader,
+      TheFooter,
+    },
+  };
+</script>
